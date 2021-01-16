936ef77c3996eaca732dfdc9044307368dae8c60
amoskal <alena.moskalenko@t-systems.com>
2021-01-16 23:03:47 +0300

    3-directives:4 description component

--- 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
+++ 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { IFeedback } from '../../../../../../shared/mocks/3-directives/product-information';
 
 @Component({
   selector: 'ngx-shop-feedbacks',
   templateUrl: './feedbacks.component.html',
   styleUrls: ['./feedbacks.component.sass'],
 })
-export class FeedbacksComponent {}
+export class FeedbacksComponent {
+  @Input()
+  public feedbacks: IFeedback[] = [];
+
+  @Output() addFeedbackEvent = new EventEmitter<string>();
+
+  public addNewFeedback(): void{
+    this.addFeedbackEvent.emit('отзыв добавлен');
+  }
+}
