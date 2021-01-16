6b5de71aec298a447877e9e1a12ada7326ae68a2
amoskal <alena.moskalenko@t-systems.com>
2021-01-16 20:46:24 +0300

    2-pipe

--- 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
@@ -6,6 +6,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 // @ts-ignore
 export class RatePipe implements PipeTransform {
   public transform(value: number): number | undefined {
-    return 0;
+    let fractPart = value % 1;
+    const intPart = Math.floor(value);
+    // console.log('value', value);
+    // console.log('fract', fractPart);
+    // console.log('intPart', intPart);
+
+    if (fractPart < 0.25) {
+      fractPart = 0;
+    }
+    else if (fractPart < 0.75) {
+      fractPart = 0.5;
+    }
+    else {
+      fractPart = 1;
+    }
+    return intPart + fractPart;
   }
 }
--- 2-pipe/1-transforms-with-pipe/src/app/pipes/review.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/review.pipe.ts
@@ -6,6 +6,27 @@ import { PipeTransform, Pipe } from '@angular/core';
 // @ts-ignore
 export class ReviewPipe implements PipeTransform {
   public transform(countOfReviews: number | undefined): string {
-    return '';
+    const enum suffixes {
+      OneReview = 'отзыв',
+      TwoReviews = 'отзыва',
+      FiveReviews = 'отзывов'
+    }
+    let suffix = '';
+
+    if (!countOfReviews) {
+      return 'Нет отзывов';
+    }
+
+    if ((countOfReviews % 10) === 1 && (countOfReviews % 100) !== 11) {
+      suffix = suffixes.OneReview; // 1, 21, 31, 121, 1021, ...
+    }
+    else if ([2, 3, 4].includes(countOfReviews % 10) && ![12, 13, 14].includes(countOfReviews % 100)) {
+      suffix = suffixes.TwoReviews; // 2, 3, 4, 22, 23, 24, 122, ...
+    }
+    else {
+      suffix = suffixes.FiveReviews;
+    }
+
+    return countOfReviews + ' ' + suffix;
   }
 }
