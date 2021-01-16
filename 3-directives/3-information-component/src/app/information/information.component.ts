307a4183bb46aa35d4d41385b9de9ff6c1f83179
amoskal <alena.moskalenko@t-systems.com>
2021-01-16 22:35:24 +0300

    3-directives:3 information component

--- 3-directives/3-information-component/src/app/information/information.component.ts
+++ 3-directives/3-information-component/src/app/information/information.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../../../shared/mocks/3-directives/product';
+import {fakeAsync} from '@angular/core/testing';
 
 @Component({
   selector: 'ngx-shop-information',
   templateUrl: './information.component.html',
 })
-export class InformationComponent {}
+export class InformationComponent {
+  @Input()
+  public product: IProduct = null;
+
+  @Output()
+  public addToCart = new EventEmitter<string>();
+
+  public isShow = false;
+
+  public addToBasket(): void {
+    this.addToCart.emit('товар добавлен в кoрзину');
+  }
+
+  public show(): void {
+    this.isShow = !this.isShow;
+  }
+
+}
