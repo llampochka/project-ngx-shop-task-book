a7266ea2929fdc2ddcdcd5d79f4e1a6e42d2c6e5
amoskal <alena.moskalenko@t-systems.com>
2021-01-20 21:06:12 +0300

    3-directives:5 category dropdown

--- 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
+++ 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICategory} from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-category-dropdown',
   templateUrl: './category-dropdown.component.html',
 })
-export class CategoryDropdownComponent {}
+export class CategoryDropdownComponent {
+
+  public currentIndex = null;
+  public currentCategory = '';
+  public currentCategoryId = '';
+
+  @Input()
+  public categories: ICategory[] = [];
+
+  @Output()
+  public subCategorySelectEvent = new EventEmitter<string>();
+
+  public showSubCategories(index: number): void {
+    this.currentIndex = index;
+  }
+
+  public isSubCategoryShown(index: number): boolean {
+    return index === this.currentIndex;
+  }
+
+  public subCategorySelect(categoryName: string, categoryId: string): void {
+    this.currentCategoryId = categoryId;
+    this.subCategorySelectEvent.emit(categoryName);
+  }
+}
